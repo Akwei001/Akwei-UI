e113ab0396f03ea8b3dd4740e19333cdd54a55f4
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-03-16 16:47:55 +0000

    Storybook working

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,13 @@
+import {ComponentProps} from "react"
 
-const Button = () => {
+type ButtonProps = ComponentProps<"button">
+
+
+
+
+const Button = ({...props}: ButtonProps) => {
   return (
-    <button className="text-blue-500">Button</button>
+    <button className="text-blue-500"{...props}>Button</button>
   )
 }
 
