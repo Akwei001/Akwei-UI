154406211ab5e262e6b9a987e74f28ee019964b0
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-03-19 21:37:58 +0000

    variant size

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,31 @@
+import { cva } from "class-variance-authority";
 import {ComponentProps} from "react"
 
+
+const buttonStyles = cva([
+  "w-full",
+"rounded-md",
+"font-semibold",
+"focus:outline-none",
+"disabled:cursor-not-allowed",],
+
+   {
+    variants: {
+      variant: {
+        solid: "",
+        outline: "border-2",
+        ghost: "transition-colors duration-300",
+      },
+       size: {
+        sm: "px-4 py-2 text-sm",
+        md: "px-4 py-2 text-base",
+        lg: "px-6 py-3 text-lg",
+      },
+   },
+  }
+);
+
 type ButtonProps = ComponentProps<"button">
-
-
 
 
 const Button = ({...props}: ButtonProps) => {
